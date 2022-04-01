cfca58aeff3b6c4a9225b33abf3e73947443553d
docteurjay <aristejean@gmail.com>
2022-04-01 00:21:43 -0400

    Securing the Catalog Microservice

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -43,14 +43,16 @@ namespace Play.Identity.Service
                         serviceSettings.ServiceName
                     );
 
-            services.AddIdentityServer(options => {
-                
+            services.AddIdentityServer(options =>
+            {
+
                 options.Events.RaiseSuccessEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseErrorEvents = true;
             })
             .AddAspNetIdentity<ApplicationUser>()
             .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
+            .AddInMemoryApiResources(identityServerSettings.ApiResources)
             .AddInMemoryClients(identityServerSettings.Clients)
             .AddInMemoryIdentityResources(identityServerSettings.IdentityResources)
             .AddDeveloperSigningCredential();
